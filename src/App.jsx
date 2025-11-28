1f3628c383d1e52cf9b3a7217ba09c3bb81f16c9
tenutayomica <micatenu@gmail.com>
2025-11-28 01:34:10 -0300

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,33 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const scrollToSection = (id) => {
+    const section = document.getElementById(id);
+    if (section) {
+      section.scrollIntoView({
+        behavior: 'smooth', 
+        block: 'start'      
+      });
+    }
+  };
   return (
     <div className='App-container'>
       <main>
         <header>
-           <img src="/TopBar.png" alt="topbar" id='topbar'/>
+          <section id='headerbtncontainer'>
+           <button id="btn menu principal" className='headerbtn' onClick={()=>scrollToSection('screen1')}>Menú Principal</button>
+           <button id="btn sobre mi" className='headerbtn'onClick={()=>scrollToSection('screen2')}>Sobre Mí</button>
+           <button id="btn proyectos" className='headerbtn' onClick={()=>scrollToSection('screen3')}>Proyectos</button>
+           <button id="btn contacto" className='headerbtn' onClick={()=>scrollToSection('screen4')}>Contacto</button>
+          </section>
+           <img src="/Rectangle 1.png" alt="topbar" id='topbar'/>
         </header>
        <section id='screen1' className='screen'>
-           <img src="/Portada (1).png" alt="portada" />
+        <section id='screen1bckgs' className='backgrounds'>
+           <img src="/Portada (1).png" alt="portada" id='portada'/>
+           <img src="/Portada (1).png" alt="portada" id='portada2'/>
+        </section>
+           
            <section id='textsec1'>
             <h1 className='screen1text'>Ariel</h1>
             <h2 className='screen1text'>Cholakian Zas</h2>
@@ -20,12 +38,41 @@ function App() {
            </section>
        </section>
        <section id='screen2' className='screen'>
+        <section id='screen2bckgs' className='backgrounds'>
+           <img src="/Sobre Mí (1).png" alt="backgroung" id='screen2-background'/>
+           <img src="/Sobre Mí (1).png" alt="backgroung" id='screen2-background2'/>
+        </section>
+         <section id='screen2-components'>
+           <img src="/sobremi.png" alt="sobremi" id='sobremi'/>
+           <img src="/habilidades.png" alt="habilidades" id='habilidades'/>
+         </section>
+      
        </section>
        <section id='screen3' className='screen'>
-
+          <img src="/Proyectos.png" alt="proyectosbckgound" id='proyectosbckground' className='backgrounds'/>
+         <section id='projectscontainer'>
+           <section id='bannersec'>
+             <img src="/screen3banner.png" alt="proyectosrecientes" id='banner'/>
+           </section>
+           <section id='projectsec'>
+              <img src="/robolingo.png" alt="robolingo" id='robolingo' />
+              <img src="/rampagearena.png" alt="rampagearena" id='rampagearena' />
+              <img src="/feufighters.png" alt="feufighters" id='feufighters' />
+           </section>
+          </section>
+         
        </section>
        <section id='screen4' className='screen'>
-
+          <img src="/Contacto.png" alt="contactosbckgound" id='contactosbckground' className='backgrounds'/>
+         <section id='contactscontainer'>
+           <section id='bannersec2'>
+             <img src="/banner2.png" alt="banner" id='banner2'/>
+           </section>
+           <section id='contactssec'>
+              <img src="/contactos.png" alt="contacts" id='contactos'/>
+           </section>
+          </section>
+         
        </section>
        </main>
     </div>
